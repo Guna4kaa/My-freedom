1ea812f460be8147413d896a0230f87d153639fc
Guna4kaa <Guna4ka@mail.ru>
2024-01-16 02:24:08 +0600

    Add new lesson JS

---
+++ JavaScript/lesson1/script.js
@@ -0,0 +1,58 @@
+// // console.log('First lesson MyFreedom');
+
+// // Создание переменной
+// // let myName = "Gulnar"
+// // console.log(myName);
+// // let - объявлять (создать) переменную (declaration)
+// // myName - название переменной
+// // = "" - присваивание значений
+
+// // camelCase - myName
+
+
+// // ПРИМИТИВНЫЕ ТИПЫ ДАННЫХ (простые)
+// // 1. STRING (строка, текст)
+// const text1 = "some text"
+
+// // 2. NUMBER (число)
+// const num1 = 5
+// const num2 = 1000
+// const num3 = 0.5
+
+// // 3. BOOLEAN (булевое, правда, ложь)
+// const isHeSmart = true
+// const isHeNotSmart = false
+
+// // 4. UNDEFINED (неопределено)
+// let someUndefinedVariable
+
+// // 5. BIG INT (большие числа)
+// const bigInt = 21324324112313n
+// console.log(typeof 5)
+// console.log(5)
+// console.log(typeof bigInt)
+// console.log(bigInt)
+
+// // 6. SYMBOL 
+// const symb = Symbol("hello")
+// console.log(symb)
+
+
+
+// НЕ РИМИТИВНЫЕ ТИПЫ ДАННЫХ (сложные)
+// 1. OBJECT 
+// const user = {
+//     name: "Gulnar",
+//     surname: "Assubekova",
+//     age: "35",
+//     sayHello: () => {console.log ('hello')} функция
+// }
+// console.log(user.sayHello ());
+
+// 1.1 - ARRAY (массив, список)
+// const students = ['Sabit', 'Gulnar', 'Serik']
+// console.log(students);
+
+// 2. NULL (объект, пустота)
+// const num1 = undefined
+// const num2 = null
